4ef9f8d42a1098d1a447a21d9cb5371c493cecd7
Alex Bugay <bosco.bugai@gmail.com>
2023-02-06 20:57:37 +0200

    first WindowsForm

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtPassword1 = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.iDiagnost = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Surname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Login";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Repeat password";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(350, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Output";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(180, 50);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtSurname.TabIndex = 6;
+            this.txtSurname.TextChanged += new System.EventHandler(this.txtSurname_TextChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(180, 100);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 7;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(180, 150);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtLogin.TabIndex = 8;
+            this.txtLogin.TextChanged += new System.EventHandler(this.txtLogin_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(180, 200);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 9;
+            // 
+            // txtPassword1
+            // 
+            this.txtPassword1.Location = new System.Drawing.Point(180, 250);
+            this.txtPassword1.Name = "txtPassword1";
+            this.txtPassword1.PasswordChar = '*';
+            this.txtPassword1.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword1.TabIndex = 10;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(450, 50);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(160, 220);
+            this.txtOutput.TabIndex = 11;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(150, 300);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 12;
+            this.btnOK.Text = "Save";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(500, 300);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // iDiagnost
+            // 
+            this.iDiagnost.AutoSize = true;
+            this.iDiagnost.ForeColor = System.Drawing.Color.Red;
+            this.iDiagnost.Location = new System.Drawing.Point(50, 275);
+            this.iDiagnost.Name = "iDiagnost";
+            this.iDiagnost.Size = new System.Drawing.Size(11, 13);
+            this.iDiagnost.TabIndex = 14;
+            this.iDiagnost.Text = "*";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.iDiagnost);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtPassword1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtPassword1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label iDiagnost;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSurname.ForeColor = System.Drawing.Color.Gray;
+            txtName.ForeColor = System.Drawing.Color.Gray;
+            txtLogin.ForeColor = System.Drawing.Color.Gray;
+        }
+
+        private bool CheckEmptyTextBox()
+        {
+            //перевірка прізвища, імені і логіну на пустоту.
+            try
+            {
+                if (txtSurname.Text.Length == 0 || txtName.Text.Length == 0 ||
+                txtLogin.Text.Length == 0)
+                    throw new Exception("Поля обовʼязкові. Введіть дані");
+                //перевірка паролей на пустоту
+                if (txtPassword.Text.Length == 0 || txtPassword1.Text.Length == 0)
+                    throw new Exception("Поля обовʼязкові. Введіть дані.");
+                iDiagnost.Text = "";
+                return true;
+            }
+            catch (Exception e)
+            {
+                iDiagnost.Text = "* Помилка у вхідних даних. " + e.Message;
+                return false;
+            }
+        }
+
+        private void txtSurname_TextChanged(object sender, EventArgs e)
+        {
+            // перевірка прізвища на наявність тільки літер без блокування клавіатури
+            this.txtSurname.ForeColor = System.Drawing.Color.Black;
+            foreach (char c in txtSurname.Text)
+            {
+                if (!char.IsLetter(c))
+                    iDiagnost.Text = "* В поле можна вводити тільки літери";
+            }
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            // перевірка імені на наявність тільки літер без блокування клавіатури
+            txtName.ForeColor = Color.Black;
+            foreach (char c in txtName.Text)
+            {
+                if (!char.IsLetter(c))
+                    iDiagnost.Text = "* В поле можна вводити тільки літери";
+            }
+        }
+
+        private void txtLogin_TextChanged(object sender, EventArgs e)
+        {
+            txtLogin.ForeColor = Color.Black;
+        }
+
+        private bool CheckLogin()
+        {
+            // перевірка логіна. Можна вводити літери, цифри, крапку, знак @
+            //застосування регулярного виразу для перевірки пошти.
+            Regex myReg1 = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            if (!myReg1.IsMatch(txtLogin.Text))
+            {
+                iDiagnost.Text = "* Помилка при введенні логіну";
+                return false;
+            }
+            return true; 
+        }
+        private bool CheckPassword()
+        {
+            // перевірка співпадіння паролів і довжини
+            try
+            {
+                if (txtPassword.Text != txtPassword1.Text)
+                    throw new Exception("Паролі не співпадають");
+                if (txtPassword.Text.Length < 6)
+                    throw new Exception("Поганий пароль. Введіть не менше 6 символів");
+                iDiagnost.Text = "";
+                return true;
+            }
+            catch (Exception e)
+            {
+                iDiagnost.Text = "* " + e.Message;
+                return false;
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (CheckEmptyTextBox() && CheckPassword() && CheckLogin())
+            {
+                string buf = txtSurname.Text + Environment.NewLine +
+                txtName.Text + Environment.NewLine +
+                txtLogin.Text + Environment.NewLine +
+                txtPassword.Text;
+                txtOutput.Text = buf;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
